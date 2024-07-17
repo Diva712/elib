903fa248a79459a93e36d5d593ba4f236f270296
Diva gupta <divagupta712@gmail.com>
2024-07-17 18:16:33 +0530

    added hash function

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //validation
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required !!")
+    return next(error);
+  }
+
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "User already exist with this email!!");
     return next(error);
   }
 
