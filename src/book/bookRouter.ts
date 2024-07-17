fa95228efcf8215b0c047a481f12aabeafe4d9ba
Diva gupta <divagupta712@gmail.com>
2024-07-17 22:02:57 +0530

    authentication in book route

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { createBook } from './bookController';
 import path from 'node:path';
 import multer from 'multer';
+import authentication from '../middlewares/authenticate';
 
 const bookRouter = express.Router();
 
@@ -11,7 +12,7 @@ const upload = multer({
   limits: {fileSize: 3e7} // 30mb
 })
 
-bookRouter.post('/', upload.fields([
+bookRouter.post('/', authentication, upload.fields([
   { name: "coverImage", maxCount: 1 },
   {name:"file" , maxCount:1},
 ]),createBook)
